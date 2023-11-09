622bea89cb3a22bc13fc890b446b91ab672da288
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2023-11-09 17:38:33 +0530

    Add files via upload

---
+++ chat_by_ravi/src/main/java/com/chat/uii/ChatByRaviApplication.java
@@ -0,0 +1,11 @@
+package com.chat.uii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatByRaviApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatByRaviApplication.class, args);
+	}
+}
